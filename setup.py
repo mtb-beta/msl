3bcf5333dd6a4dcb6dbe9ea72dbd89afbf824b72
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 00:13:47 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='msl',
+    version='0.0.1',
+    description='My Standard Library',
+    author='Tatsuya Matoba',
+    author_email='tatsuya.matoa.wk.jp@gmail.com',
+    packages = ['msl'],
+    include_package_data=True,
+    install_requires=[],
+)
