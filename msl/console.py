8f869b6bb8eb59e55761da0ed1e9dac59074cc03
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 01:36:15 +0900

    refactoring

--- msl/console.py
+++ msl/console.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG)
 
+DATA_DIR = Path.home() / '.msl/'
+
 def main():
     logging.debug('call main')
     args = sys.argv
@@ -13,9 +15,10 @@ def main():
 
     if len(args) > 1 and sys.argv[1] == 'create':
         logging.debug('call create command')
+
         temporary_note = str(uuid.uuid1())
-        note_path = Path('~/.msl/', temporary_note)
-        (Path.home() / '.msl/').mkdir(exist_ok=True)
+        note_path = DATA_DIR / temporary_note
+        DATA_DIR.mkdir(exist_ok=True)
         logging.debug(note_path)
         os.system("vim {}".format(note_path))
 
