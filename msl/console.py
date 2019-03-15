31d06b4524c0b8208d3c2c881e7232b298dd0e00
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 01:58:28 +0900

    ノートに作成日時つけて保存する

--- msl/console.py
+++ msl/console.py
@@ -2,7 +2,9 @@ import os
 import sys
 import uuid
 import logging
+import json
 from pathlib import Path
+from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -17,10 +19,25 @@ def main():
         logging.debug('call create command')
 
         temporary_note = str(uuid.uuid1())
-        note_path = DATA_DIR / temporary_note
+        temporary_note_path = DATA_DIR / temporary_note
         DATA_DIR.mkdir(exist_ok=True)
-        logging.debug(note_path)
-        os.system("vim {}".format(note_path))
+        logging.debug(temporary_note_path)
+        # open note
+        os.system("vim {}".format(temporary_note_path))
+
+        # save note
+        if temporary_note_path.exists():
+            logging.debug('save note')
+            data = {}
+            with temporary_note_path.open() as note_file:
+                data['content'] = note_file.readline()
+
+            data['created_at'] = datetime.now().isoformat(timespec='seconds')
+            json_note_path = DATA_DIR / str(temporary_note + '.json')
+            with json_note_path.open(mode='w') as json_file:
+                json.dump(data, json_file)
+
+            os.remove(temporary_note_path)
 
 if __name__=='__main__':
     main()
