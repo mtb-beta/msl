80af8a7096366607f20cd0d34a827041e87cf96a
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 01:30:18 +0900

    ノートの配置先のデフォルトを ~/msl/に

--- msl/console.py
+++ msl/console.py
@@ -2,6 +2,7 @@ import os
 import sys
 import uuid
 import logging
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,7 +14,10 @@ def main():
     if len(args) > 1 and sys.argv[1] == 'create':
         logging.debug('call create command')
         temporary_note = str(uuid.uuid1())
-        os.system("vim {}".format(temporary_note))
+        note_path = Path('~/.msl/', temporary_note)
+        (Path.home() / '.msl/').mkdir(exist_ok=True)
+        logging.debug(note_path)
+        os.system("vim {}".format(note_path))
 
 if __name__=='__main__':
     main()
