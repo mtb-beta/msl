ae305c95f20870fe827fd8c075151cf6191110fd
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 01:09:22 +0900

    msl create ってコマンドを使えるようにする。

--- msl/console.py
+++ msl/console.py
@@ -1,7 +1,15 @@
+import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
-    print('call main')
+    logging.debug('call main')
+    args = sys.argv
+    logging.debug('sys.args:%s'% args)
 
+    if len(args) > 1 and sys.argv[1] == 'create':
+        logging.debug('call create command')
 
 if __name__=='__main__':
     main()
