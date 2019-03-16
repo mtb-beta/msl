f516c2960e4525d0259542649289d3c708d79ad5
mtb_beta <mtb.toya0403@gmail.com>
2019-03-16 13:13:57 +0900

    リファクタリング

--- msl/console.py
+++ msl/console.py
@@ -16,11 +16,17 @@ META_DIR = BASE_DIR / 'meta'
 NOTE_DIR = BASE_DIR / 'notes'
 
 def open_note(note_name):
+    """
+    This will open editor by note_name.
+    """
     NOTE_DIR.mkdir(exist_ok=True)
     note_path = NOTE_DIR / note_name
     os.system("vim {}".format(note_path))
 
 def save_meta_data(note_name):
+    """
+    This will update meta data on note_name.
+    """
     data = {}
     data['created_at'] = datetime.now().isoformat(timespec='seconds')
     note_path = NOTE_DIR / note_name
@@ -31,6 +37,52 @@ def save_meta_data(note_name):
     json_note_path = META_DIR / str(note_name + '.json')
     with json_note_path.open(mode='w') as json_file:
         json.dump(data, json_file)
+
+
+def create_command():
+    """
+    This command craete note.
+    """
+    temporary_note_name = str(uuid.uuid1())
+    BASE_DIR.mkdir(exist_ok=True)
+    # open note
+    open_note(temporary_note_name)
+
+    # save note
+    temporary_note_path = NOTE_DIR / temporary_note_name
+    if temporary_note_path.exists():
+        logging.debug('save note')
+        # save meta data
+        save_meta_data(temporary_note_name)
+
+def list_command():
+    """
+    This command list notes.
+    """
+    notes = list(NOTE_DIR.glob('*'))
+    for note in notes:
+        name = note.name
+        meta_path = META_DIR / str(name + '.json')
+        with meta_path.open() as f:
+            meta = json.load(f)
+        title = meta['title']
+        print(f'{note.name}{Fore.GREEN}:{Style.RESET_ALL}{title}')
+
+def edit_command(note_name):
+    """
+    This will open editor at note_name.
+    """
+    notes = list(NOTE_DIR.glob(note_name))
+    logging.debug(f'notes:{notes}')
+    if not notes:
+        print(f'{Fore.RED}{args[1]} is not found.{Style.RESET_ALL}')
+
+    note_name = notes[0].name
+    logging.debug(f'note_name:{note_name}')
+
+    open_note(note_name)
+    save_meta_data(note_name)
+
 
 def main():
     print(f'{Style.RESET_ALL}', end="")
@@ -45,44 +97,16 @@ def main():
 
     if len(args) > 1 and sys.argv[1] == 'create':
         logging.debug('call create command')
+        create_command()
 
-        temporary_note_name = str(uuid.uuid1())
-        BASE_DIR.mkdir(exist_ok=True)
-        # open note
-        open_note(temporary_note_name)
-
-        # save note
-        temporary_note_path = NOTE_DIR / temporary_note_name
-        if temporary_note_path.exists():
-            logging.debug('save note')
-            # save meta data
-            save_meta_data(temporary_note_name)
-        exit()
-
-    if len(args) > 1 and sys.argv[1] == 'list':
+    elif len(args) > 1 and sys.argv[1] == 'list':
         logging.debug('call list command')
-        notes = list(NOTE_DIR.glob('*'))
-        for note in notes:
-            name = note.name
-            meta_path = META_DIR / str(name + '.json')
-            with meta_path.open() as f:
-                meta = json.load(f)
-            title = meta['title']
-            print(f'{note.name}{Fore.GREEN}:{Style.RESET_ALL}{title}')
-        exit()
+        list_command()
 
-    if len(args) == 2:
+    elif len(args) == 2:
         logging.debug('call edit command')
-        notes = list(NOTE_DIR.glob(args[1]))
-        logging.debug(f'notes:{notes}')
-        if not notes:
-            print(f'{Fore.RED}{args[1]} is not found.{Style.RESET_ALL}')
-
-        note_name = notes[0].name
-        logging.debug(f'note_name:{note_name}')
-
-        open_note(note_name)
-        save_meta_data(note_name)
+        note_name = args[1]
+        edit_command(note_name)
 
 
 if __name__=='__main__':
